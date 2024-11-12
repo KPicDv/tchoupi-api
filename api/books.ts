4688fe8b63ff292184f84af71ada8463ee42fd38
KPic <kevinpichon@outlook.fr>
2024-11-12 15:06:30 +0100

    Correction paramètre

--- api/books.ts
+++ api/books.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { BOOKS, BOUGHT_BOOKS_NUMBERS } from '../contants/books';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const books = Object.entries(BOOKS).map((key, name) => ({
+  const books = Object.entries(BOOKS).map(([key, name]) => ({
     name,
     isBought: BOUGHT_BOOKS_NUMBERS.includes(Number(key)),
   }));
