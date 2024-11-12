ae3b30d7f2b1a632a2c992cd5beb781b118dbd8b
KPic <kevinpichon@outlook.fr>
2024-11-12 15:18:20 +0100

    Envoi du numéro

--- api/books.ts
+++ api/books.ts
@@ -4,6 +4,7 @@ import { BOOKS, BOUGHT_BOOKS_NUMBERS } from '../constants/books';
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const books = Object.entries(BOOKS).map(([key, name]) => ({
     name,
+    number: Number(key),
     isBought: BOUGHT_BOOKS_NUMBERS.includes(Number(key)),
   }));
   return res.json(books);
