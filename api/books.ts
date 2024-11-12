ba06743d13118bcb5c41746f52078608ca0a2605
KPic <kevinpichon@outlook.fr>
2024-11-12 15:55:20 +0100

    Gestion des CORS

--- api/books.ts
+++ api/books.ts
@@ -2,6 +2,8 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { BOOKS, BOUGHT_BOOKS_NUMBERS } from '../constants/books';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   const books = Object.entries(BOOKS).map(([key, name]) => ({
     name,
     number: Number(key),
