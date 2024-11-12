340800c87e44795a529563c0597e65e099e720dd
KPic <kevinpichon@outlook.fr>
2024-11-12 15:00:10 +0100

    Récupération des livres

---
+++ api/books.ts
@@ -0,0 +1,10 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { BOOKS, BOUGHT_BOOKS_NUMBERS } from '../contants/books';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const books = Object.entries(BOOKS).map((key, name) => ({
+    name,
+    isBought: BOUGHT_BOOKS_NUMBERS.includes(Number(key)),
+  }));
+  return res.json(books);
+}
